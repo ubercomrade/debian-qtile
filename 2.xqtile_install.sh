fd1e1934e5923fcc9fff121c1afe79c72a80a3b8
Anton Tsukanov <44977397+ubercomrade@users.noreply.github.com>
2024-03-11 19:57:21 +0700

    Update 2.xqtile_install.sh

--- 2.xqtile_install.sh
+++ 2.xqtile_install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #Xorg + Qtile
-sudo apt update
-sudo apt install pipx
-sudo apt install xserver-xorg xinit
-sudo apt install libpangocairo-1.0-0
-sudo apt install python3-pip python3-xcffib python3-cairocffi
+sudo apt install -y pipx
+sudo apt install -y xserver-xorg xinit
+sudo apt install -y libpangocairo-1.0-0
+sudo apt install -y python3-pip python3-xcffib python3-cairocffi
 pipx install qtile
 
 # Xsessions Desktop file
