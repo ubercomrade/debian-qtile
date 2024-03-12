d0f086f8fcdddbdee4d0646118ff147b0a82a7ac
Anton Tsukanov <44977397+ubercomrade@users.noreply.github.com>
2024-03-12 12:11:42 +0700

    Update 0.preparation.sh

--- 0.preparation.sh
+++ 0.preparation.sh
@@ -7,16 +7,18 @@ xdg-user-dirs-update
 sudo apt update
 
 #Tools
-sudo apt install -y neofetch vim ranger mc alacrity
+sudo apt install -y neofetch vim ranger mc alacritty
 
 #Audio
 sudo apt install -y wireplumber pipewire-pulse pipewire-alsa pipewire-audio-client-libraries pipewire-media-session-
 systemctl --user --now enable wireplumber.service
+mkdir -p /etc/pipewire/media-session.d/
 sudo touch /etc/pipewire/media-session.d/with-jack
+mkdir -p /etc/ld.so.conf.d/
 sudo cp /usr/share/doc/pipewire/examples/ld.so.conf.d/pipewire-jack-*.conf /etc/ld.so.conf.d/
 sudo ldconfig
 
 #Fonts
-sudo apt install -y fonts-jetbrains-mono fonts-font-awesome fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus
+sudo apt install -y fonts-jetbrains-mono fonts-font-awesome fonts-liberation2 fonts-liberation fonts-terminus
 
 
