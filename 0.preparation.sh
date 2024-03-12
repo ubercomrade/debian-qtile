3ee43ed6972c3f6d2f1072bf3907e864861cb52d
Anton Tsukanov <44977397+ubercomrade@users.noreply.github.com>
2024-03-12 11:34:07 +0700

    Update 0.preparation.sh

--- 0.preparation.sh
+++ 0.preparation.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+#Create standart directories for user
 xdg-user-dirs-update
+
+#Update
 sudo apt update
-sudo apt install -y neofetch vim ranger 
+
+#Tools
+sudo apt install -y neofetch vim ranger mc alacrity
+
+#Audio
+sudo apt install -y wireplumber pipewire-pulse pipewire-alsa pipewire-audio-client-libraries pipewire-media-session-
+systemctl --user --now enable wireplumber.service
+sudo touch /etc/pipewire/media-session.d/with-jack
+sudo cp /usr/share/doc/pipewire/examples/ld.so.conf.d/pipewire-jack-*.conf /etc/ld.so.conf.d/
+sudo ldconfig
+
+#Fonts
+sudo apt install -y fonts-jetbrains-mono fonts-font-awesome fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus
 
 
